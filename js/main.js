1516a9fc91ecc47b118d47753c4522d1ea6d65f3
kacperchachula <kacper.chachula@outlook.com>
2023-05-22 18:12:01 +0200

    finished website

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const nav = document.querySelector('.nav')
 const navBtn = document.querySelector('.burger-btn')
 const allNavItems = document.querySelectorAll('.nav__item')
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
 	nav.classList.toggle('nav--active')
@@ -18,6 +21,8 @@ const handleNav = () => {
 const handleNavItemsAnimation = () => {
 	let delayTime = 0
 
+	navBtnBars.classList.remove('black-bars-color');
+
 	allNavItems.forEach(item => {
 		item.classList.toggle('nav-items-animation')
 		item.style.animationDelay = '.' + delayTime + 's'
@@ -25,7 +30,6 @@ const handleNavItemsAnimation = () => {
 	})
 }
 
-navBtn.addEventListener('click', handleNav)
 
 const deleteAnimation = () => {
 	allNavItems.forEach(item => {
@@ -36,3 +40,26 @@ const deleteAnimation = () => {
 		})
 	})
 }
+
+
+const handleObserver = () => {
+	const currentSection = window.scrollY;
+
+	allSections.forEach(section => {
+		if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+            navBtnBars.classList.add('black-bars-color')
+        } else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+            navBtnBars.classList.remove('black-bars-color')
+        }
+	})
+}
+
+const handleCurrentYear = () => {
+	const year = (new Date).getFullYear();
+	footerYear.innerText = year;
+}
+
+handleCurrentYear();
+navBtn.addEventListener('click', handleNav)
+window.addEventListener('scroll', handleObserver)
+
